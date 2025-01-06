ffd08aff88d0837de026c071044aea1ff44e5726
Omar Allouch <allouchomar1@gmail.com>
2025-01-06 17:15:54 +0100

    [back] remove update todo

--- backend/routes/todo.ts
+++ backend/routes/todo.ts
@@ -22,17 +22,6 @@ router.post("/", async (req, res) => {
 	res.status(201).json(newTodo);
 });
 
-// Update a todo
-router.put("/:id", async (req, res) => {
-	const { id } = req.params;
-	const { title, description, dueDate, status } = req.body;
-	const updatedTodo = await prisma.todo.update({
-		where: { id: Number(id) },
-		data: { title, description, dueDate, status },
-	});
-	res.json(updatedTodo);
-});
-
 // Delete a todo
 router.delete("/:id", async (req, res) => {
 	const { id } = req.params;
