16bc7ed1a027b88e0c194e8487facb69bc7878fd
Omar Allouch <allouchomar1@gmail.com>
2025-01-07 11:59:15 +0100

    [docker] use docker network instead of localhost

--- cli/source/cli.tsx
+++ cli/source/cli.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { render, Box, Text, useInput } from "ink";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:4000/todos";
+const API_BASE_URL = process.env['API_BASE_URL'] || "http://localhost:4000/todos";
 
 interface Todo {
 	id: number;
